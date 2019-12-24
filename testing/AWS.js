c71591871dec858b921be3bf1d0ecd6a083da664
Felipe Rohde <feliperohde@MacBook-Pro-de-Felipe.local>
2019-12-24 14:08:56 -0300

    removec CIRCLE_CI env

--- testing/AWS.js
+++ testing/AWS.js
@@ -1,12 +1,13 @@
 const AWS = require('aws-sdk');
+const production = process.env.NODE_ENV === 'production';
 
 AWS.config.update({
-	accessKeyId: process.env.CIRCLECI ? process.env.accessKeyId : 'spec',
-	secretAccessKey: process.env.CIRCLECI ? process.env.secretAccessKey : 'spec',
+	accessKeyId: production ? process.env.accessKeyId : 'spec',
+	secretAccessKey: production ? process.env.secretAccessKey : 'spec',
 	region: 'us-east-1'
 });
 
-const dynamoDbClient = new AWS.DynamoDB(process.env.CIRCLECI ? {} : {
+const dynamoDbClient = new AWS.DynamoDB(production ? {} : {
 	endpoint: 'http://localhost:9090'
 });
 
